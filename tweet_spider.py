beeb495285ee34ef4ab0e5e1f493b1718aa789a9
zhenghy <zhenghy723@126.com>
2018-04-12 21:55:16 -0400

    delete print

--- tweet_spider.py
+++ tweet_spider.py
@@ -46,7 +46,6 @@ def search_twitter():
 					md5Set.add(MD5value);
 					if '\u2026' not in tweet["text"]:
 						res_list.append(tweet);
-						print(tweet["text"]);
 			
 			#result = news_data.insert_many(res_list);
 			#print(result);
