16e7e7db7a6d8ad6cf4a719102210907d67e8430
zhenghy <zhenghy723@126.com>
2018-04-20 14:19:37 -0400

    make dat spider more robust

--- data_spider.py
+++ data_spider.py
@@ -15,14 +15,15 @@ def getPrice():
 		cur2 = "USD";
 		url = "https://cex.io/api/last_price/" + cur1 + "/" + cur2;
 		response = requests.get(url);
-		content = response.json();
-		data = {}
-		data['id'] = '1';
-		data['price'] = content['lprice'];
-		data['timestamp'] = int(time.time());
-		data['exchange'] = 'cex';
-		price_data.insert_one(data);
-		print(data);
+		if response is not None and response.status_code == 200:
+			content = response.json();
+			data = {}
+			data['id'] = '1';
+			data['price'] = content['lprice'];
+			data['timestamp'] = int(time.time());
+			data['exchange'] = 'cex';
+			price_data.insert_one(data);
+			print(data);
 		time.sleep(30);
 
 
